be00fce25d41e7a05474f5f4c7b634babc9cd16e
priyanka-0978 <136354473+priyanka-0978@users.noreply.github.com>
2025-06-19 20:07:34 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Step 1: Install system dependency for PyAudio
-apt-get update && apt-get install -y portaudio19-dev
-
-# Step 2: Install Python dependencies
-pip install -r requirements.txt
